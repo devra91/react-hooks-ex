b5f54020a44237f8146d0a1bf6af2adc28388fb3
Keesung CHOI <keesung@Keesungui-MacBookPro.local>
2020-06-19 15:17:56 +0900

    useState 여러번 사용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Counter from './Counter';
+import Info from './Info';
 
 const App = () => {
-  return <Counter />
+  return <Info /> 
+  // <Counter />
 };
 
 export default App;
